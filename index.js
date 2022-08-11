363be498f9352b634f4fdf1724bfdb2be775a1a6
emericgautier <emericgtr@gmail.com>
2022-08-11 10:59:32 +0200

    create function

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 let totalSeconds;
+let interval;
+
+function countDown() {
+  const minutes = Math.floor(totalSeconds / 60);
+  // Le reste de seconde qui ne rentre pas dans les minutes
+  const seconds = totalSeconds % 60;
+
+  // est-ce que seconds est inférieur à 10 ? alors... sinon...
+  const sec = seconds < 10 ? "0" + seconds : seconds;
+
+  // affichage du compte à rebours, ajouter les variables
+  countdownDisplay.textContent = `${minutes} : ${sec}`;
+  // si totalSeconds est supérieur à 0, alors tu décrémentes
+  if (totalSeconds > 0) {
+    totalSeconds--;
+  } else {
+    countdownDisplay.textContent = "C'est terminé !";
+    clearInterval(interval);
+  }
+}
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -8,6 +28,7 @@ form.addEventListener("submit", (e) => {
   } else {
     totalSeconds = choice.value * 60;
     choice.value = "";
-    setInterval(countDown, 1000);
+    clearInterval(interval);
+    interval = setInterval(countDown, 1000);
   }
 });
